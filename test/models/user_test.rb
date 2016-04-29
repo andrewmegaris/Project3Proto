603e40deb97bd9853cec139e42c982f17d8c538d
Andrew Megaris <andrew.megaris@gmail.com>
2016-04-29 12:09:37 +0000

    started fixing testing, switching branch

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,35 +3,35 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Test User", email: "test@email.com",
+                     password: "password", password_confirmation: "password")
   end
   
-  test "should be valid" do
+  test "user should be valid" do
     assert @user.valid?
   end
   
-  test "name should be present" do
+  test "user should have a name" do
     @user.name = "    "
     assert_not @user.valid?
   end
 
-  test "email should be present" do
+  test "user should have an email" do
     @user.email = "    "
     assert_not @user.valid?
   end
   
-  test "name should not be too long" do
-    @user.name = "a" * 51 
+  test "user name less than 48 char" do
+    @user.name = "z" * 48 
     assert_not @user.valid?
   end
   
-  test "email should not be too long" do 
-    @user.email = "a" * 244 + "@example.com"
+  test "user base email less than 216 char" do 
+    @user.email = "z" * 216 + "@example.com"
     assert_not @user.valid?
   end
   
-  test "email validation should accept valid addresses" do
+  test "mailer validation should take valid adresses" do
     valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org
                       first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -40,7 +40,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
   
-  test "email validation should reject invalid addresses" do
+  test "mailer validation should NOT take invalid adresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
                           foor@bar_baz.com foor@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -49,19 +49,19 @@ class UserTest < ActiveSupport::TestCase
     end
   end
   
-  test "email should be unique" do
+  test "user should have unique email" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
   
-  test "password should be present (nonblank)" do
+  test "user cannot have blank password" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
   
-  test "password should have a minimum length" do 
+  test "user password must be 6 or more char" do 
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
