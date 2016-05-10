b8df5e92ad72d3dc9245e80e478e74955cae3126
Rusty Riedel <rusty.riedel@ku.edu>
2016-05-09 20:46:38 -0500

    Update status message.

--- server scripts/start_server.sh
+++ server scripts/start_server.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
 #this script launches the server on port 80 for http
-echo starting server...
+echo Starting Hawk Cloud server...
 cd project3proto
 rvmsudo rails server --binding=0.0.0.0 --port=80
